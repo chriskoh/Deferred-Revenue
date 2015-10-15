b0a9d69a07cdebd6a08cc5a13c7998313d78e1ca
Chris Koh <chris.koh90@gmail.com>
2015-10-15 09:11:58 -0700

    Create ArrayTrim.js

---
+++ ArrayTrim.js
@@ -0,0 +1,18 @@
+function trim(arr)
+{
+	var i,
+	len=arr.length,
+	out=[],
+	obj={};
+
+	for (i=0;i<len;i++) 
+	{
+		obj[arr[i]]=0;
+	}
+	for (i in obj) 
+	{
+		out.push(i);
+	}
+	
+	return out;
+}
