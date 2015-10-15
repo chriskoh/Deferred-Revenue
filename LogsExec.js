f20f3d6fda01b1c532b8cba3a1aea42061ff8f8c
Chris Koh <chris.koh90@gmail.com>
2015-10-15 09:11:43 -0700

    Create LogsExec.js

---
+++ LogsExec.js
@@ -0,0 +1,6 @@
+function logx(name, value)
+{	
+	var context        = nlapiGetContext();
+	var usageRemaining = context.getRemainingUsage();
+	nlapiLogExecution ('DEBUG', name + ' | ' + usageRemaining, value);
+}
